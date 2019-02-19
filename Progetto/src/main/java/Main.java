b90a9640c974d38edd2ccae470ec33ed62dfedcc
Luca Ambrosio <luca.ambrosio@student.supsi.ch>
2019-02-19 23:06:40 +0100

    aggiunta del algoritmo 2 opt e foglio ecel per il calcolo dell'errore

--- Progetto/src/main/java/Main.java
+++ Progetto/src/main/java/Main.java
@@ -3,8 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    private static List<Citta> cittaList;
+
     public static void main(String[] args){
-        List<Citta> cittaList = new ArrayList<>();
+        cittaList = new ArrayList<>();
         Integer dimesione=0;
         System.out.println("ciao mondo!!!");
         String nome=args[0];
@@ -28,16 +31,30 @@ public class Main {
                     }
                 }else{
                     //System.out.println(line);
-                    String[] splittata=line.split(" ");
-                    Integer numero_riga=Integer.parseInt(splittata[0]);
-                    Double coordx=Double.parseDouble(splittata[1]);
-                    Double coordy= Double.parseDouble(splittata[2]);
-                    System.out.println("il numero della riga e\'"+numero_riga);
-                    System.out.println("la coordinata X e\' : "+coordx);
-                    System.out.println("la coordinata Y e\' : "+coordy);
-                    System.out.println();
-                    //aggiungo le varie città
-                    cittaList.add(new Citta(numero_riga,coordx,coordy));
+                    try{
+                        String[] splittata=line.split(" ");
+                        Integer numero_riga=Integer.parseInt(splittata[0]);
+                        Double coordx=Double.parseDouble(splittata[1]);
+                        Double coordy= Double.parseDouble(splittata[2]);
+                        System.out.println("il numero della riga e\'"+numero_riga);
+                        System.out.println("la coordinata X e\' : "+coordx);
+                        System.out.println("la coordinata Y e\' : "+coordy);
+                        System.out.println();
+                        //aggiungo le varie città
+                        cittaList.add(new Citta(numero_riga,coordx,coordy));
+                    }catch (NumberFormatException e){
+                        String[] splittata=line.split(" ");
+                        Integer numero_riga=Integer.parseInt(splittata[1]);
+                        Double coordx=Double.parseDouble(splittata[2]);
+                        Double coordy= Double.parseDouble(splittata[3]);
+                        System.out.println("il numero della riga e\'"+numero_riga);
+                        System.out.println("la coordinata X e\' : "+coordx);
+                        System.out.println("la coordinata Y e\' : "+coordy);
+                        System.out.println();
+                        //aggiungo le varie città
+                        cittaList.add(new Citta(numero_riga,coordx,coordy));
+                    }
+
 
                 }
             }
@@ -50,7 +67,7 @@ public class Main {
         Integer[][] distanze=new Integer[dimesione][dimesione];
         for(int i=0;i<dimesione;i++){
             for(int j=0;j<dimesione;j++){
-                distanze[i][j]=distanza(cittaList.get(i),cittaList.get(j));
+                distanze[i][j]=distanza(cittaList.get(i), cittaList.get(j));
             }
         }
 
@@ -70,27 +87,63 @@ public class Main {
         //nella variabile viaggio avrò i vari nodi suoi quali passare
         //adesso stampiamo il viaggio che il nostro algoritmo ha calcolato
         //con il nostro NN il viaggio migliore e\':
+        List<Citta> cittaviaggio= new ArrayList<Citta>();
         for(int i=0;i<viaggio.size();i++){
             System.out.println(viaggio.get(i));
+            cittaviaggio.add(cittaList.get(viaggio.get(i)));
         }
-        Integer distanzatotale=0;
+        Integer distanzatotale= 0;
 
+        distanzatotale = lunghezzaviaggio(viaggio);
+        System.out.println("la distanza totale e\' : "+distanzatotale);
+        //in viaggio ho il viaggio con NN
+        //adesso eseguo l'algoritmo twoopt
+        List<Citta> migliore=TwoOpt.alternate(cittaviaggio);
+        System.out.println("la lunghezza con il 2 opt algoritmo e\' : "+ lunghezzaviaggiocitta(migliore));
+
+    }
+
+    public static Integer lunghezzaviaggio(List<Integer> viaggio) {
+        Integer distanzatotale=0;
         for(int i=0;i<cittaList.size();i++){
             System.out.println("la"+i+ " citta\' e\' :" + viaggio.get(i));
             if(i!=cittaList.size()-1){
                 Integer distanza =0;
                 distanza=distanza(cittaList.get(i),cittaList.get(i+1));
                 distanzatotale+=distanza;
-                System.out.println("la distanza tra "+ i + " e " + i+1 +" e\' "+distanza );
+                //System.out.println("la distanza tra "+ i + " e " + i+1 +" e\' "+distanza );
+            }else{
+                System.out.print("");
+                Integer distanza =0;
+                distanza=distanza(cittaList.get(0),cittaList.get(i));
+                distanzatotale+=distanza;
             }
 
 
         }
-        System.out.println("la distanza totale e\' : "+distanzatotale);
+        return distanzatotale;
+    }
 
+
+    public static Integer lunghezzaviaggiocitta(List<Citta> viaggio) {
+        Integer distanzatotale=0;
+        for(int i=0;i<cittaList.size();i++){
+            if(i!=cittaList.size()-1){
+                Integer distanza =0;
+                distanza=distanza(viaggio.get(i),viaggio.get(i+1));
+                distanzatotale+=distanza;
+            }else{
+                Integer distanza =0;
+                distanza=distanza(viaggio.get(0),viaggio.get(i));
+                distanzatotale+=distanza;
+            }
+
+
+        }
+        return distanzatotale;
     }
     //funzione che calcola le distanze
-    private static Integer distanza(Citta citta, Citta citta1) {
+    public static Integer distanza(Citta citta, Citta citta1) {
         //distanza euclidea
         return (int) Math.round(Math.sqrt(Math.pow(citta.getCoordx()-citta1.getCoordx(),2)+Math.pow(citta.getCoordy()-citta1.getCoordy(),2)));
     }
